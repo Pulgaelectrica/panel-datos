01d0458d15713870fd6e7d988707e269395dd98f
Pulgaelectrica <Alfonso.orban@hotmail.com>
2025-11-07 04:08:53 +0100

    Usar fetch global en función Finnhub

--- netlify/functions/finnhub-proxy.js
+++ netlify/functions/finnhub-proxy.js
@@ -1,14 +1,11 @@
-const fetch = require("node-fetch");
-
 exports.handler = async function(event, context) {
   const symbol = event.queryStringParameters.symbol;
   const apiKey = "d46ks21r01qgc9etb0o0d46ks21r01qgc9etb0og"; // Tu API Finnhub
 
-  const url = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${apiKey}`;
-
   try {
-    const response = await fetch(url);
-    const data = await response.json();
+    // Usando fetch global de Node 22
+    const res = await fetch(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${apiKey}`);
+    const data = await res.json();
 
     return {
       statusCode: 200,
